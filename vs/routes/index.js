9e07f91d0e4992ca0916db8092a9bb7b7378557d
anirudh khade <anirudh24io010@satiengg.in>
2024-05-31 20:36:28 -0700

    still stuck on readfile

--- vs/routes/index.js
+++ vs/routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
+const fs=require('fs');
+const path=require('path')
+
+const gpath=path.join(__dirname,'../','public','models')
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+
+router.get('/', function(req, res, next) {
+  const files=fs.readFileSync(gpath);
+  res.render('index', {files:files});
+});
+
+
+
+router.post('/createfile', function(req, res, next) {
+  const{filename}=req.body;
+  fs.writeFileSync(path.join(gpath,filename),'' )
+  res.redirect('/');
 });
 
 module.exports = router;
